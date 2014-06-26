b4cf35e215a5641614510386c48c80b3e01debca
John Moon <john.moon@livingsocial.com>
2014-06-25 21:30:35 -0400

    RESTful Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
 
-   root 'projects#index'
+  root 'projects#index'
 
   devise_for :users
-  # root 'posts#index'
-
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :feedbacks, only: [:create]
-    resources :votes, only: [:create, :destroy]
-  end
 end
