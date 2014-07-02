1d9c09c7bf3ce5738a9e26d90f6f0ad02c93a5b7
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-07-02 13:43:39 -0400

    Included aasm_states on the Project Model, using the status column

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,38 @@
 class Project < ActiveRecord::Base
+  include AASM
+
   validates_presence_of :name
   validates_presence_of :description
-  validates_presence_of :status
   validates_presence_of :organizer
+  validates_presence_of :status
+
+  # AASM
+  aasm :column => 'status' do
+    state :pending_approval, :initial => true
+    state :approved
+    state :looking_for_contributors
+    state :in_progress
+    state :completed
+    state :cancelled
+
+    event :approve do
+      transitions :from => :pending_approval, :to => :approved
+    end
+
+    event :find_contributors do
+      transitions :from => :approved, :to => :looking_for_contributors
+    end
+
+    event :start_progress do
+      transitions :from => [:approved, :looking_for_contributors], :to => :in_progress
+    end
+
+    event :complete do
+      transitions :from => [:approved, :looking_for_contributors, :in_progress], :to => :completed
+    end
+
+    event :cancel do
+      transitions :from => [:pending_approval, :approved, :looking_for_contributors, :in_progress], :to => :cancelled
+    end
+  end
 end
