d7d92029ecea06eecfce3b0dbe9acfa090f0b56b
John Moon <john.moon@livingsocial.com>
2014-08-12 22:41:46 -0400

    Add landing page for users to understand what the website is about
    Added colored boxes around introductory text

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   resources :projects
 
   resources :projects do
-    resources :comments, only: [:create]
+    resources :comments, :only => [:create]
   end
 
-  root 'projects#index'
+  resources :home, :only => [:index]
+
+  root 'home#index'
 
   devise_for :users, :controllers => {
     :omniauth_callbacks => "users/omniauth_callbacks",
