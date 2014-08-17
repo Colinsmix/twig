b99d835f24fbe7f169243a8b87a085183bcf6c6a
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2014-08-17 12:43:12 -0400

    Added Assignment Model, a join between Users and Roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140814182247) do
+ActiveRecord::Schema.define(version: 20140817163606) do
+
+  create_table "assignments", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "role_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -54,7 +61,8 @@ ActiveRecord::Schema.define(version: 20140814182247) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "full_name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
